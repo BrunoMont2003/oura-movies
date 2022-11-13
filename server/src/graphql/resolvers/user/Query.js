49c70d9945e232582bc93e73c10220e8f4f62afe
brunomont <brunoadrian.mont@gmail.com>
2022-11-12 19:40:21 -0500

    add: populating favorite movies on getting users 🔥

--- server/src/graphql/resolvers/user/Query.js
+++ server/src/graphql/resolvers/user/Query.js
@@ -2,11 +2,15 @@ import { GraphQLYogaError } from '@graphql-yoga/node'
 import User from '../../../models/User'
 const userQueries = {
   getUsers: async () => {
-    const users = await User.find().select('-password')
+    const users = await User.find()
+      .select('-password')
+      .populate('favorites_movies')
     return users
   },
   getUser: async (_, { id }) => {
-    const user = await User.findById(id).select('-password')
+    const user = await User.findById(id)
+      .select('-password')
+      .populate('favorites_movies')
     if (user) {
       return user
     } else {
