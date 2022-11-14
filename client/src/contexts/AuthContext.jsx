2f282d331cdf629dcf4dd2d249a2540d50248e6a
brunomont <brunoadrian.mont@gmail.com>
2022-11-13 21:51:41 -0500

    feat: add auth context provider and hook

---
+++ client/src/contexts/AuthContext.jsx
@@ -0,0 +1,44 @@
+import { createContext, useState, useEffect, useContext } from 'react'
+import { useQuery } from '@apollo/client'
+import GET_ME from '../graphql/queries/users'
+
+export const AuthContext = createContext()
+
+export const AuthProvider = ({ children }) => {
+  const [isAuth, setIsAuth] = useState(false)
+  const [user, setUser] = useState(null)
+  const { data } = useQuery(GET_ME)
+
+  useEffect(() => {
+    const token = window.localStorage.getItem('token')
+    if (token) {
+      setIsAuth(true)
+      fetchUser()
+    }
+  }, [])
+
+  const loginUser = (token) => {
+    window.localStorage.setItem('token', token)
+    setIsAuth(true)
+  }
+
+  const logoutUser = () => {
+    window.localStorage.removeItem('token')
+    setUser(null)
+    setIsAuth(false)
+  }
+
+  const fetchUser = () => {
+    if (data) {
+      setUser(data.me)
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, isAuth, loginUser, logoutUser }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export const useAuthContext = () => useContext(AuthContext)
