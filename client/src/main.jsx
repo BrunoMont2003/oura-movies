139b29871f60ffe16c1809dccfd3d8a7552ccfdc
brunomont <brunoadrian.mont@gmail.com>
2022-11-13 21:50:18 -0500

    add: toast container

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,22 @@ import { RouterProvider } from 'react-router-dom'
 import { ApolloProvider } from '@apollo/client'
 import client from './apolloClient'
 import './assets/styles/index.css'
+import 'react-toastify/dist/ReactToastify.css'
 import router from './router'
+import { AuthProvider } from './contexts/AuthContext'
+import { ToastContainer } from 'react-toastify'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ApolloProvider client={client}>
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+        <ToastContainer
+          theme='dark'
+          closeButton
+          closeOnClick
+          autoClose={false}
+        />
+      </AuthProvider>
     </ApolloProvider>
   </StrictMode>
 )
