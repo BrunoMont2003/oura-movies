6348a6de73aca7b1aec2105efe263b565df6db62
brunomont <brunoadrian.mont@gmail.com>
2022-11-14 15:36:47 -0500

    add: popular section on carousel

--- client/src/components/Popular/index.jsx
+++ client/src/components/Popular/index.jsx
@@ -1,14 +1,14 @@
-import { GET_MOVIES } from '../../graphql/queries/movies'
+import { GET_POPULAR_MOVIES } from '../../graphql/queries/movies'
 import { useQuery } from '@apollo/client'
 import MovieCard from '../MovieCard'
 import Spinner from '../Spinner'
 
 function Popular () {
-  const { loading, error, data } = useQuery(GET_MOVIES)
+  const { loading, error, data } = useQuery(GET_POPULAR_MOVIES)
   return (
-    <div className='bg-neutral-800 w-screen p-5 relative lg:px-16'>
-      <h2 className='text-2xl mb-5'>Popular on Oura</h2>
-      <section className='flex gap-2 overflow-x-scroll overflow-y-visible py-5'>
+    <div className='w-screen p-5 relative lg:px-16'>
+      <h2 className='text-2xl font-bold'>Popular on Oura</h2>
+      <section className='flex gap-2 overflow-x-scroll overflow-y-visible py-5' style={{ scrollbarWidth: 'none' }}>
         {loading
           ? (
             <div className='min-h-[300px] w-full flex items-center justify-center bg-neutral-900'>
@@ -17,10 +17,10 @@ function Popular () {
             )
           : error
             ? (
-              <p>Error :(</p>
+              <div className='bg-neutral-800 flex items-center justify-center'>Something went Wrong</div>
               )
             : (
-                data.getMovieCatalogs.map((movie) => (
+                data.getPopularMovies.slice(0, 10).map((movie) => (
                   <MovieCard key={movie.id} movie={movie} />
                 ))
               )}
