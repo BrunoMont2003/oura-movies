02391ef53e6c068ce304afaf1abe600d6ee79904
brunomont <brunoadrian.mont@gmail.com>
2022-11-13 22:01:49 -0500

    add: redirection when is login

--- client/src/layouts/guest/index.jsx
+++ client/src/layouts/guest/index.jsx
@@ -1,12 +1,21 @@
 import GuestBg from '../../components/GuestBg'
 import GuestNavbar from '../../components/GuestNavbar'
-
+import { useAuthContext } from '../../contexts/AuthContext'
+import { useNavigate } from 'react-router-dom'
 function GuestLayout ({ children }) {
+  const { isAuth } = useAuthContext()
+  const navigate = useNavigate()
+  if (isAuth) {
+    navigate('/')
+  }
+
   return (
     <div className='min-h-screen bg-neutral-800 bg-opacity-80'>
       <GuestNavbar />
       <GuestBg />
-      <main className='min-h-screen w-screen flex items-center justify-center'>{children}</main>
+      <main className='min-h-screen w-screen flex items-center justify-center'>
+        {children}
+      </main>
     </div>
   )
 }
--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,7 +1,12 @@
 import Portal from '../../components/Portal'
 import GuestLayout from '../../layouts/guest'
-
+import { useAuthContext } from '../../contexts/AuthContext'
+import AuthenticatedLayout from '../../layouts/authenticated'
 function Home () {
+  const { isAuth } = useAuthContext()
+  if (isAuth) {
+    return <AuthenticatedLayout />
+  }
   return (
     <GuestLayout>
       <Portal />
