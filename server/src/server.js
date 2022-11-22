d2f8c65cf14337864f48374e3f10f37c412ddddb
brunomont <brunoadrian.mont@gmail.com>
2022-11-21 21:39:07 -0500

    add param plugins on yoga

--- server/src/server.js
+++ server/src/server.js
@@ -1,34 +1,20 @@
-// import { createServer } from '@graphql-yoga/node'
-// import resolvers from './graphql/resolvers/'
-// import { loadSchemaSync } from '@graphql-tools/load'
-// import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
 import { decodeTokenAndGetUser } from './helpers/token'
 import { schema } from './graphql/schema'
 import { createYoga } from 'graphql-yoga'
 import { createServer } from 'http'
-// const server = createServer({
-//   schema: {
-//     typeDefs: loadSchemaSync('./src/**/*.graphql', {
-//       loaders: [new GraphQLFileLoader()]
-//     }),
-//     resolvers
-//   },
-//   context: async ({ request }) => {
-//     const token = request ? request.headers.get('authorization') : null
-//     if (token) {
-//       const user = await decodeTokenAndGetUser(token)
-//       return { currentUser: user }
-//     }
-//   },
+import plugins from './graphql/plugins'
 
-//   maskedErrors: {
-//     handleParseErrors: true,
-//     handleValidationErrors: true
-//   }
-// })
-
-const yoga = createYoga({ schema })
-
+const yoga = createYoga({
+  schema,
+  plugins,
+  context: async ({ request }) => {
+    const token = request ? request.headers.get('authorization') : null
+    if (token) {
+      const user = await decodeTokenAndGetUser(token)
+      return { currentUser: user }
+    }
+  }
+})
 const server = createServer(yoga)
 
 export default server
