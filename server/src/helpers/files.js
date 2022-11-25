c4e3ebbcd5b438705b1ccdaadfc6bc1058f82981
brunomont <brunoadrian.mont@gmail.com>
2022-11-25 16:56:24 -0500

    add: helper to save a file from data base64

---
+++ server/src/helpers/files.js
@@ -0,0 +1,14 @@
+import fs from 'fs'
+import path from 'path'
+export const saveBase64File = (base64, fileName) => {
+  fileName = fileName.replace(/[^a-zA-Z0-9.]/g, '-')
+  const data = base64.replace(/^data:image\/\w+;base64,/, '')
+  const buffer = Buffer.from(data, 'base64')
+  const filePath = path.join(__dirname, `../../public/images/${fileName}`)
+  fs.writeFileSync(filePath, buffer)
+  return filePath
+}
+
+export const deleteFile = (filePath) => {
+  fs.unlinkSync(filePath)
+}
