8a61f376deeb063e18f053cfdd936e91727dd38f
brunomont <brunoadrian.mont@gmail.com>
2022-11-28 21:16:12 -0500

    add: no scrollbar class, support to chrome and firefox

--- client/src/components/TopRated/index.jsx
+++ client/src/components/TopRated/index.jsx
@@ -8,7 +8,7 @@ function TopRated () {
   return (
     <div className='w-100 p-5 relative lg:px-16'>
       <h2 className='text-2xl font-bold'>Top Rated</h2>
-      <section className='flex gap-2 overflow-x-scroll overflow-y-visible py-5' style={{ scrollbarWidth: 'none' }}>
+      <section className='flex gap-2 overflow-x-scroll overflow-y-visible py-5 no-scrollbar'>
         {loading
           ? (
             <div className='min-h-[300px] w-full flex items-center justify-center bg-neutral-900'>
