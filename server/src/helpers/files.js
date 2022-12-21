33eb3bd703d7bf861e0cd22ea01528a1b944d751
brunomont <brunoadrian.mont@gmail.com>
2022-12-20 21:04:26 -0500

    (server) add: create a tmp folder if not exists

--- server/src/helpers/files.js
+++ server/src/helpers/files.js
@@ -4,6 +4,10 @@ export const saveBase64File = (base64, fileName) => {
   fileName = fileName.replace(/[^a-zA-Z0-9.]/g, '-')
   const data = base64.replace(/^data:image\/\w+;base64,/, '')
   const buffer = Buffer.from(data, 'base64')
+  // create tmp folder if not exists
+  if (!fs.existsSync(path.join(__dirname, '../../tmp'))) {
+    fs.mkdirSync(path.join(__dirname, '../../tmp'))
+  }
   const filePath = path.join(__dirname, `../../tmp/${fileName}`)
   fs.writeFileSync(filePath, buffer)
   return filePath
